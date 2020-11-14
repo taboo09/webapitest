d884ca3432a151175e3f01aaa233eff3d2eb4dce
taboo09 <marius.stanciu@outlook.com>
2020-11-14 20:35:59 +0000

    address completed

--- Models/Address.cs
+++ Models/Address.cs
@@ -3,5 +3,9 @@ namespace webapitest.Models
     public class Address
     {
         public int AddressId { get; set; }
+        public int Number { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
     }
 }
