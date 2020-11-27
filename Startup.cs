158a923842f3daaaac37a7dd212c6b98a74c5501
Marius <marius_taboo@yahoo.com>
2020-11-27 21:46:19 +0000

    add swagger generation

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,14 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace wepapitest
+namespace webapitest
 {
     public class Startup
     {
@@ -19,6 +23,8 @@ namespace wepapitest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            AddSwaggerGeneration(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,9 +41,49 @@ namespace wepapitest
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction => 
+            {
+                setupAction.SwaggerEndpoint(
+                    "/swagger/v1/swagger.json",
+                    "Web Test API"
+                );
+                setupAction.RoutePrefix = "";
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private static void AddSwaggerGeneration(IServiceCollection services)
+        {
+            services.AddSwaggerGen(setup => 
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo()
+                    {
+                        Title = "Web App Test Api",
+                        Version = "1",
+                        Description = "Through this API you can access Users and Companies.",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                        {
+                            Email = "webapitest@gmail.com",
+                            Name = "Marius Stanciu",
+                            Url = new Uri("https://www.linkedin.com/in/mariusstanciu/")
+                        },
+                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                        {
+                            Name = "MIT License",
+                            Url = new Uri("https://opensource.org/licenses/MIT")
+                        }
+                    });
+
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+                setup.IncludeXmlComments(xmlCommentsFullPath);
             });
         }
     }
